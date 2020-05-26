b1a13b6cc0a66ad73a5eca13ceb8104d7c8dfaab
jlferrete <joseluisferrete@gmail.com>
2020-05-26 19:18:37 +0200

    Added global configurations

---
+++ src/app/config.js
@@ -0,0 +1,17 @@
+/*=============================================
+Exportamos la ruta para tomar imágenes
+=============================================*/
+export let Path = {
+
+	url: 'http://localhost:4200/assets/'
+
+}
+
+/*=============================================
+Exportamos el endPoint de la APIREST de Firebase
+=============================================*/
+export let Api = {
+
+	url: 'https://marketplace-201f4.firebaseio.com/'
+
+}
